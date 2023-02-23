8ec7954e06fecb8c6738b1c3e875b9ff7ca3dd21
martindylan <dylanmartin1922@gmail.com>
2023-02-23 04:05:35 -0300

    changed email text for img and fixed gh img aspect ratio

--- src/Foot/Foot.tsx
+++ src/Foot/Foot.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Foot.module.css';
-import logoGithub from './img/logo-github.png';
-import logoInstagram from './img/logo-instagram.png';
 import logoBandcamp from './img/logo-bandcamp.png';
 import logoYoutube from './img/logo-youtube.png';
 import logoSpotify from './img/logo-spotify.png';
+import logoGithub from './img/logo-github.png';
+import logoInstagram from './img/logo-instagram.png';
+import logoEmail from './img/logo-email.png';
 
 type Props = {
   display?: string
@@ -15,14 +16,14 @@ export default function Foot(props: Props) {
     <div className={styles.this}>
       <div className={`${props.display}`}>
         <div className={styles.links}>
-          <a href='https://github.com/martindylan/' target='_blank' rel='noreferrer' ><img src={logoGithub} alt='github'></img></a>
-          <a href='https://instagram.com/0.36787944117/' target='_blank' rel='noreferrer'><img src={logoInstagram} alt='instagram'></img></a>
-          <div><span>dylanmartin1922@gmail.com</span></div>
-        </div>
-        <div className={styles.links}>
           <a href='https://dylan-martin.bandcamp.com/' target='_blank' rel='noreferrer'><img src={logoBandcamp} alt='bandcamp'></img></a>
           <a href='https://youtube.com/@martindylan' target='_blank' rel='noreferrer'><img src={logoYoutube} alt='youtube'></img></a>
           <a href='https://open.spotify.com/artist/28Iq0vqHr3bvU4H1FnVPqk?' target='_blank' rel='noreferrer'><img src={logoSpotify} alt='spotify'></img></a>
+        </div>
+        <div className={styles.links}>
+          <a href='https://github.com/martindylan/' target='_blank' rel='noreferrer' ><img src={logoGithub} alt='github'></img></a>
+          <a href='https://instagram.com/0.36787944117/' target='_blank' rel='noreferrer'><img src={logoInstagram} alt='instagram'></img></a>
+          <a href='mailto:dylanmartin1922@gmail.com' target='_blank' rel='noreferrer'><img src={logoEmail} alt='email'></img></a>
         </div>
       </div>
     </div>
