a63f4e1d64bc58ec14ad6ef6210cb77455b38df6
martindylan <dylanmartin1922@gmail.com>
2023-02-23 03:44:58 -0300

    removed old comments

--- src/DropdownProvider.tsx
+++ src/DropdownProvider.tsx
@@ -1,4 +1,3 @@
-// import { createContext, useContext, useReducer, ReactNode } from "react";
 import { createContext, useContext, useState, ReactNode } from "react";
 
 const DropdownContext = createContext<any>({});
@@ -7,39 +6,12 @@ const useDropdownContext = () => {
   return useContext(DropdownContext);
 }
 
-// const initialState = {
-//   isDropped: false
-// }
-
-// type ActionType = {
-//   type: 'isDropped',
-//   // value: Boolean
-// }
-
-// const reducer = (state: typeof initialState, action: ActionType) => {
-
-//   switch (action.type) {
-//     case 'isDropped':
-//       return {
-//         ...state,
-//         // isDropped: action.value
-//         isDropped: !state.isDropped
-//       };
-//     default:
-//       return {
-//         ...state,
-//         isDropped: false
-//       };
-//   }
-// }
-
 type Props = {
   children?: ReactNode
 }
 
 const DropdownProvider = (props: Props) => {
 
-  // const [state, dispatch] = useReducer(reducer, initialState);
   const [state, setState] = useState(false);
 
   return (
