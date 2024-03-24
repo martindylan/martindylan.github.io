e7be36a8a8cb2bf781680bc9e1f7d71b7a619098
martindylan <dylanmartin1922@gmail.com>
2024-03-24 13:20:05 -0300

    removed spotify link, readded bandcamp link

--- src/Foot/Foot.tsx
+++ src/Foot/Foot.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Foot.module.css';
-import logoSpotifySmall from './img/logo-spotify-small.png';
+import logoBandcamp from './img/logo-bandcamp.png';
 import logoGithub from './img/logo-github.png';
 import logoInstagram from './img/logo-instagram.png';
 
@@ -14,8 +14,8 @@ export default function Foot(props: Props) {
       <div className={`${props.display}`}>
         <div className={styles.links}>
           <a href='https://github.com/martindylan/' target='_blank' rel='noreferrer' ><img src={logoGithub} alt='github'></img></a>
-          <a href='https://open.spotify.com/artist/28Iq0vqHr3bvU4H1FnVPqk/' target='_blank' rel='noreferrer' ><img src={logoSpotifySmall} alt='github'></img></a>
           <a href='https://instagram.com/0.36787944117/' target='_blank' rel='noreferrer'><img src={logoInstagram} alt='instagram'></img></a>
+          <a href='https://dylan-martin.bandcamp.com/' target='_blank' rel='noreferrer' ><img src={logoBandcamp} alt='bandcamp'></img></a>
         </div>
       </div>
     </div>
