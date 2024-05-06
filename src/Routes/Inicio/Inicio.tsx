8faeccc14992bc5d9c39c3433260219cd3ed9fd0
martindylan <dylanmartin1922@gmail.com>
2024-05-06 20:50:26 -0300

    feat: refactor home i18n

--- src/Routes/Inicio/Inicio.tsx
+++ src/Routes/Inicio/Inicio.tsx
@@ -5,48 +5,29 @@ import { useTranslation } from 'react-i18next';
 
 export default function Inicio() {
   const [t, i18n] = useTranslation("global");
-
+  const blocks = t("home.blocks", { returnObjects: true }) as Array<any>;
   return (
     <div className={styles.this}>
       <Title>
         {t("home.title")}<span className='col1'>*･°</span>
       </Title>
-      <div className={styles.block}>
-        <div className={styles.line}>
-          {t("home.blocks.0.lines.0")}
+      {blocks.map((block) => (
+        <div className={styles.block}>
+          {block.lines.map((line: string) => (
+            <div className={styles.line}>
+              <div>{line}</div>
+              { }
+            </div>
+          ))}
+          {block.items && (
+            <ul>
+              {block.items.map((item: string) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          )}
         </div>
-      </div>
-      <div className={styles.block}>
-        <div className={styles.line}>
-          <div>{t("home.blocks.1.lines.0")}</div>
-        </div>
-        <div className={styles.line}>
-          <div>{t("home.blocks.1.lines.1")}</div>
-        </div>
-        <div className={styles.line}>
-          <div>{t("home.blocks.1.lines.2")}</div>
-        </div>
-      </div>
-      <div className={styles.block}>
-        <div className={styles.line}>
-          <div>{t("home.blocks.2.lines.0")}</div>
-        </div>
-      </div>
-      <div className={styles.block}>
-        <div className={styles.line}>
-          <div>{t("home.blocks.3.lines.0")}:</div>
-          <ul>
-            <li>{t("home.blocks.3.items.0")}</li>
-            <li>{t("home.blocks.3.items.1")}</li>
-            <li>{t("home.blocks.3.items.2")}</li>
-            <li>{t("home.blocks.3.items.3")}</li>
-            <li>{t("home.blocks.3.items.4")}</li>
-            <li>{t("home.blocks.3.items.5")}</li>
-            <li>{t("home.blocks.3.items.6")}</li>
-            <li>{t("home.blocks.3.items.7")}</li>
-          </ul>
-        </div>
-      </div>
+      ))}
       <div className={styles.block}>
         <div className={styles.line}>
           <div> _/</div>
